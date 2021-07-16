7c4c93c110aef15acd6063883e102476f19b06d4
mangojuicedeli <mgkang0317@gmail.com>
2021-07-16 18:58:31 +0900

    feat: api gateway 서비스 추가

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -6,8 +6,8 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final Environment environment;
     private final Greeting greeting;
     private final UserService userService;
 
@@ -29,7 +30,8 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return "It's working in user-service.";
+        return String.format("It's working in user-service on port %s.",
+                environment.getProperty("local.server.port"));
     }
 
     @PostMapping("/users")
